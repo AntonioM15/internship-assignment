62fc02348ec8cdecbc09b7061970b150120d1d98
Antonio <anmarlea.cs@gmail.com>
2024-11-17 20:46:00 +0100

    #4 - Frontend - Actualizar los tests para que pasen

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -3,6 +3,6 @@
 describe('Home Test', () => {
   it('Landing page loads', () => {
     cy.visit('http://localhost:8080/landing');
-    cy.contains('Portal Prácticas de Vuelo');
+    cy.contains('Portal Prácticas de Ciclos');
   });
 });
