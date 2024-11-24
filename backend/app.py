3c74f09b8768aecb807605584f80b77b8072f745
Antonio <anmarlea.cs@gmail.com>
2024-11-24 17:10:42 +0100

    #5 - Añadir un blueprint de prueba - Nuevo blueprint testing_users.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 from routes.landing import landing_blueprint
+from routes.testing_users import testing_users_blueprint
 
 
 app = Flask(__name__,
@@ -11,11 +12,9 @@ app = Flask(__name__,
 app.config["MONGO_URI"] = "mongodb://localhost:27017/tfm_local_db"
 mongo = PyMongo(app)
 
-
-# Register all blueprints (files where endpoints are located)
-app.register_blueprint(landing_blueprint, url_prefix="/api/v1/landing")
 # Register all blueprints with deferred initialization in order to share the same mongodb
 app.register_blueprint(landing_blueprint(mongo), url_prefix="/api/v1/landing")
+app.register_blueprint(testing_users_blueprint(mongo), url_prefix="/api/v1/testing_users")
 
 
 @app.route('/', defaults={'path': ''})
