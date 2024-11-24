aff0fb97ea1e716966a72291d0e398909e6c57da
Antonio <anmarlea.cs@gmail.com>
2024-11-24 17:10:02 +0100

    #5 - Utilizar PyMongo en los blueprints - Actualizar los ficheros para que puedan hacer uso de la variable mongo

--- backend/routes/landing.py
+++ backend/routes/landing.py
@@ -1,46 +1,48 @@
 from flask import Blueprint, jsonify, request
 
 # Blueprint definition
-landing_blueprint = Blueprint('landing_blueprint', __name__)
+landing = Blueprint('landing_blueprint', __name__)
 
 
-# TODO remove example endpoint
-@landing_blueprint.route('/message', methods=["GET"])
-def create_task():
-    return jsonify('Hola mundo desde Flask')
+def landing_blueprint(mongo):
+    mongo_db = mongo.db
 
+    # TODO remove example endpoint
+    @landing.route('/message', methods=["GET"])
+    def create_task():
+        return jsonify('Hola mundo desde Flask')
 
-@landing_blueprint.route('/login', methods=["POST"])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-    if not email or not password:
-        return jsonify({"status": "error", "message": "Email o contraseña no proporcionado"}), 400
+    @landing.route('/login', methods=["POST"])
+    def login():
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+        if not email or not password:
+            return jsonify({"status": "error", "message": "Email o contraseña no proporcionado"}), 400
 
-    # TODO mocked response until DB is configured
-    if email != 'aa' or password != 'bb':
-        return jsonify({"status": "error", "message": "Email o contraseña incorrectos"}), 401
+        # TODO mocked response until DB is configured
+        if email != 'aa' or password != 'bb':
+            return jsonify({"status": "error", "message": "Email o contraseña incorrectos"}), 401
 
-    return jsonify({"status": "success"}), 200
+        return jsonify({"status": "success"}), 200
 
+    @landing.route('/password-recovery', methods=["POST"])
+    def recover_password():
+        data = request.get_json()
+        email = data.get('email')
+        if not email:
+            return jsonify({"status": "error", "message": "Email no proporcionado"}), 400
 
-@landing_blueprint.route('/password-recovery', methods=["POST"])
-def recover_password():
-    data = request.get_json()
-    email = data.get('email')
-    if not email:
-        return jsonify({"status": "error", "message": "Email no proporcionado"}), 400
+        # TODO mocked response until DB is configured
+        if email != 'aa':
+            return jsonify({"status": "error", "message": "Email incorrecto"}), 401
 
-    # TODO mocked response until DB is configured
-    if email != 'aa':
-        return jsonify({"status": "error", "message": "Email incorrecto"}), 401
+        # TODO send email to reset credentials
+        return jsonify({"status": "success", "message": "Email con instrucciones enviado"}), 200
 
-    # TODO send email to reset credentials
-    return jsonify({"status": "success", "message": "Email con instrucciones enviado"}), 200
+    @landing.route('/contact', methods=["GET"])
+    def provide_contact():
+        # TODO
+        return jsonify({"status": "success"}), 200
 
-
-@landing_blueprint.route('/contact', methods=["GET"])
-def provide_contact():
-    # TODO
-    return jsonify({"status": "success"}), 200
+    return landing
