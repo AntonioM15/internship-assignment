d6f4dd658a7274f9dd637dd77f038e850c56a65e
Antonio <anmarlea.cs@gmail.com>
2025-06-26 20:18:51 +0200

    #45 - Actualizar la NavBar para soportar cerrar sesión

--- frontend/src/components/generic/NavBar.vue
+++ frontend/src/components/generic/NavBar.vue
@@ -14,6 +14,19 @@
           {{ link.name }}
         </router-link>
       </li>
+      <li class="spacer"></li> <!-- Spacer to push logout to the right -->
+      <li class="nav-option" v-for="option in options" :key="option.name">
+        <router-link :to="option.path" class="nav-set">
+          <img
+            class="option"
+            :src="currentIcon(option)"
+            :alt="option.name"
+            @mouseover="hoverIcon = option.hoverIcon"
+            @mouseleave="hoverIcon = null"
+          />
+          {{ option.name }}
+        </router-link>
+      </li>
     </ul>
   </nav>
 
@@ -30,24 +43,29 @@ import IconCompanyLight from '../../assets/IconCompanyLight.svg'
 import IconCompanyDark from '../../assets/IconCompanyDark.svg'
 import IconAssignmentsLight from '../../assets/IconAssignmentsLight.svg'
 import IconAssignmentsDark from '../../assets/IconAssignmentsDark.svg'
+import IconLogOutLight from '../../assets/IconLogOutLight.svg'
+import IconLogOutDark from '../../assets/IconLogOutDark.svg'
 
 export default {
   name: 'NavBar',
   data () {
     return {
-      hoverIcon: null, // Save the icon that it's being hovered
+      hoverIcon: null, // Save the icon or option that it's being hovered
       links: [
         { name: 'Inicio', path: '/dashboard', icon: IconDashboardLight, hoverIcon: IconDashboardDark },
         { name: 'Estudiantes', path: '/students', icon: IconStudentLight, hoverIcon: IconStudentDark },
         { name: 'Tutores', path: '/tutors', icon: IconProfessorLight, hoverIcon: IconProfessorDark },
         { name: 'Empresas', path: '/companies', icon: IconCompanyLight, hoverIcon: IconCompanyDark },
         { name: 'Asignaciones', path: '/assignments', icon: IconAssignmentsLight, hoverIcon: IconAssignmentsDark }
+      ],
+      options: [
+        { name: '', path: '/logout', icon: IconLogOutLight, hoverIcon: IconLogOutDark }
       ]
     }
   },
   methods: {
     currentIcon (link) {
-      // Return hover icon if active, otherwise return default icon
+      // Return hover icon/option if active, otherwise return default icon/option
       return this.hoverIcon === link.hoverIcon ? link.hoverIcon : link.icon
     }
   }
