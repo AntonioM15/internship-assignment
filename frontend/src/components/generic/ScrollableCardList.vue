a0b63f36cc243ecb060e8d9bce21ef484c1fa65a
Antonio <anmarlea.cs@gmail.com>
2025-08-09 16:59:43 +0200

    #19 - Nuevo componente ScrollableCardList - Incluye nuevos estilos e icono

---
+++ frontend/src/components/generic/ScrollableCardList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="scrollable-box">
+    <div v-for="(item, index) in items" :key="index" class="card-item">
+      <div class="icon-box">
+        <slot name="icon" :item="item">
+          <img :src="defaultIcon" alt="Avatar" />
+        </slot>
+      </div>
+      <div class="item-text">
+        <slot name="content" :item="item">
+          {{ item.description }}
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconUserDefault from '../../assets/IconUserDefault.svg'
+export default {
+  name: 'ScrollableCardList',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      defaultIcon: IconUserDefault
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('../../css/ScrollableCardList.css');
+</style>
