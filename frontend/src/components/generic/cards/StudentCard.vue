6dbb163097f6d6931cdbb7445932f7722daeb56f
Antonio <anmarlea.cs@gmail.com>
2025-08-14 19:28:05 +0200

    #53 - Añadir tipos para los ScrollableCardList.vue

---
+++ frontend/src/components/generic/cards/StudentCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card-item">
+    <div class="icon-box">
+      <slot name="icon" :item="item">
+        <img :src="defaultIcon" alt="Avatar" />
+      </slot>
+    </div>
+    <div class="item-text">
+      <div class="item-title">
+        <slot name="title" :item="item">
+          {{ item.full_name }}
+        </slot>
+      </div>
+      <div class="item-text">
+        <slot name="degree" :item="item">
+          <!-- TODO - Update to json methods to show the actual degree name -->
+          {{ item.degree }}
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconUserDefault from '../../../assets/IconUserDefault.svg'
+
+export default {
+  name: 'StudentCard',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      defaultIcon: IconUserDefault
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('../../../css/Cards.css');
+</style>
