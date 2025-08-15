9aed85c8860e26f079c51023ebb43e0559ddf656
Antonio <anmarlea.cs@gmail.com>
2025-08-15 13:49:35 +0200

    #53 - Listar los estudiantes

--- frontend/src/components/Students.vue
+++ frontend/src/components/Students.vue
@@ -3,11 +3,18 @@
   <div class="container">
     <Header/>
     <NavBar/>
+    <ActionBar/>
     <div v-if="error" style="color: red;">Error: {{ error }}</div>
     <div v-else-if="loading">Cargando...</div>
-    <ul v-else>
-      <li v-for="(item, index) in data" :key="index">{{ item }}</li>
-    </ul>
+    <div v-else class="students-layout">
+      <section class="left-panel">
+        <div class="card-list">
+          <ScrollableCardList :items="students" :kind="'students'" />
+        </div>
+      </section>
+      <section class="right-panel">
+      </section>
+    </div>
   </div>
 
 </template>
@@ -16,25 +23,30 @@
 import apiUrl from '../config'
 import Header from './generic/Header.vue'
 import NavBar from './generic/NavBar.vue'
+import ScrollableCardList from './generic/ScrollableCardList.vue'
+import ActionBar from './generic/ActionBar.vue'
 import axios from 'axios'
 
 export default {
   name: 'Students',
   components: {
     Header,
-    NavBar
+    NavBar,
+    ScrollableCardList,
+    ActionBar
   },
   data () {
     return {
       loading: true,
-      error: null
+      error: null,
+      students: []
     }
   },
   mounted () {
     const path = `${apiUrl}/api/v1/students`
     axios.get(path)
       .then(response => {
-        this.data = response.data.data
+        this.students = response.data.data.students
       })
       .catch(err => {
         this.error = err.response.data.message || err.message
